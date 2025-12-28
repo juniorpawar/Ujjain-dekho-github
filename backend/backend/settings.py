b8d2117369cbfeb91ff3151c5cfe204db3a5dd25
yuvraj singh pawar <yuvrajsingh3641@gmail.com>
2025-12-28 16:22:50 +0530

    Update Django settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-p3w#cu=q1b1evwd(m408cj#a=k**)!w877!n3txdyv=toyras)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ujjaindekho.in','www.ujjaindekho.in']
 
 
 # Application definition
